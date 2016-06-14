141c61f156eec8af00e42c77029c0528e70524ea
sbridge <sbridge@gmail.com>
2016-06-14 10:24:35 +1000

    comments

---
+++ ScriptEngine/ScriptView/QuickMVVM.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Quick.MVVM
+{
+	/// <summary>
+	/// quick and dirty implementation of the relay command pattern
+	/// </summary>
+	public class RelayCommand : ICommand
+	{
+		Func<bool>     _canExecute;
+		Action<object> _execute;
+
+		/// <summary>
+		/// construct an always can execute relay command with the given action
+		/// </summary>
+		/// <param name="execute"></param>
+		public RelayCommand(Action<object> execute)
+		{
+			this._canExecute = () => true;
+			this._execute = execute;
+		}
+
+		public RelayCommand(Func<bool> canExecute, Action<object> execute)
+		{
+			this._canExecute = canExecute;
+			this._execute = execute;
+
+			// pass through the requery event:
+			CommandManager.RequerySuggested += (s, e) => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		public bool CanExecute(object parameter)
+		{
+			return _canExecute();
+		}
+
+		public void Execute(object parameter)
+		{
+			_execute(parameter);
+		}
+	}
+
+	/// <summary>
+	/// very basic view-model base
+	/// </summary>
+	public class SimpleViewModel : INotifyPropertyChanged
+	{
+		protected Dictionary<string, object> _values = new Dictionary<string, object>();
+
+		protected T Get<T>(string name)
+		{
+			return (T)this[name];
+		}
+
+		/// <summary>
+		/// gets or sets a value from the dictionary
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		protected object this[string name]
+		{
+			get
+			{
+
+				object tmp = null;
+				if (_values.TryGetValue(name, out tmp))
+					return tmp;
+				else
+					return null;
+			}
+			set { _values[name] = value; OnPropertyChanged(name); }
+		}
+
+		/// <summary>
+		/// fires the PropertyChanged event for all values.
+		/// </summary>
+		protected void RefreshAll()
+		{
+			foreach (var k in _values.Keys)
+			{
+				OnPropertyChanged(k);
+			}
+		}
+
+		public string WindowTitle
+		{
+			get { return this[nameof(WindowTitle)] as string; }
+			set { this[nameof(WindowTitle)] = value; }
+		}
+
+		public string ToolTip
+		{
+			get { return this[nameof(ToolTip)] as string; }
+			set { this[nameof(ToolTip)] = value; }
+		}
+
+		public Cursor Cursor
+		{
+			get { return this[nameof(Cursor)] as Cursor; }
+			set { this[nameof(Cursor)] = value; }
+		}
+
+		public bool IsBusy
+		{
+			get { return (bool)this[nameof(IsBusy)]; }
+			set {
+
+				this[nameof(IsBusy)] = value;
+				if (value)
+				{
+					this.Cursor = Cursors.Wait;
+				}
+				else
+				{
+					this.Cursor = Cursors.Arrow;
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string name)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		#region Safe Invokation
+
+		/// <summary>
+		/// invokes an action on the UI thread
+		/// </summary>
+		/// <param name="action"></param>
+		public void SafeInvoke(Action action)
+		{
+			System.Windows.Application.Current.Dispatcher.Invoke(action);
+		}
+
+		/// <summary>
+		/// invokes an action on the UI thread and returns a result.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="callback"></param>
+		/// <returns></returns>
+		public T SafeInvoke<T>(Func<T> callback)
+		{
+			return System.Windows.Application.Current.Dispatcher.Invoke<T>(callback);
+		}
+
+		#endregion
+
+		/// <summary>
+		/// returns true if the application is in design-mode.
+		/// </summary>
+		public bool InDesignMode
+		{
+			get
+			{
+				return (bool)DependencyPropertyDescriptor.FromProperty(DesignerProperties.IsInDesignModeProperty, typeof(FrameworkElement)).Metadata.DefaultValue;
+			}
+		}
+	}
+
+	/// <summary>
+	/// base class for a tree-view-model;
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class TreeViewModel<T> : TreeViewModel
+	{
+		/// <summary>
+		/// the root nodes collection
+		/// </summary>
+		public ObservableCollection<T> Nodes { get; set; } = new ObservableCollection<T>();
+		
+	}
+
+	/// <summary>
+	/// base class for a tree-view-model;
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class TreeViewModel : SimpleViewModel
+	{
+		public TreeViewModel()
+		{
+			this.Position = Dock.Left;
+			this.Visible = Visibility.Collapsed;
+			this.IsBusy = false;
+			this.IsExpanded = false;
+			this.IsSelected = false;
+		}
+
+		public string Name
+		{
+			get { return this[nameof(Name)] as string; }
+			set { this[nameof(Name)] = value; }
+		}
+
+		public Dock Position
+		{
+			get { return (Dock)this[nameof(Position)]; }
+			set { this[nameof(Position)] = value; }
+		}
+
+		public Visibility Visible
+		{
+			get { return (Visibility)this[nameof(Visible)]; }
+			set
+			{
+				this[nameof(Visible)] = value;
+			}
+		}
+
+		public Action<SimpleViewModel> OnSelected { get; set; }
+		public Action<SimpleViewModel> OnExpanded { get; set; }
+
+		public ICommand Show { get { return new RelayCommand(() => Visible != Visibility.Visible, (o) => this.Visible = Visibility.Visible); } }
+
+		public ICommand Hide { get { return new RelayCommand(() => Visible == Visibility.Visible, (o) => this.Visible = Visibility.Collapsed); } }
+
+		
+		public bool IsSelected
+		{
+			get { return (bool)this[nameof(IsSelected)]; }
+			set
+			{
+				this[nameof(IsSelected)] = value;
+				if (value)
+				{
+					if (OnSelected != null)
+						OnSelected.Invoke(this);
+				}
+			}
+		}
+
+		public bool IsExpanded
+		{
+			get { return (bool)this[nameof(IsExpanded)]; }
+			set
+			{
+
+				this[nameof(IsExpanded)] = value;
+				if (value)
+				{
+					if (OnExpanded != null)
+						OnExpanded.Invoke(this);
+				}
+			}
+		}
+	}
+}
---
+++ ScriptEngine/ScriptView/SqlSources.cs
@@ -0,0 +1,512 @@
+﻿using Quick.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptView
+{
+	
+
+	/// <summary>
+	/// information about an SQL server instance.
+	/// </summary>
+	public class SqlServerInfo :  IEquatable<SqlServerInfo>
+	{
+		/// <summary>
+		/// returns the description of the server/
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			return this.Description;
+		}
+
+		/// <summary>
+		/// gets a connection string for this SQL server
+		/// </summary>
+		protected string ConnectionString
+		{
+
+			get
+			{
+				return $"Data Source ={DataSource};Integrated Security=True;Connection Timeout=1";
+			}
+		}
+
+		/// <summary>
+		/// gets the DataSource part of the connection string.
+		/// </summary>
+		protected string DataSource
+		{
+			get
+			{
+
+				if (!string.IsNullOrEmpty(InstanceName))
+					return $"{ServerName}\\{InstanceName}";
+				else
+					return ServerName;
+
+			}
+		}
+
+		/// <summary>
+		/// construct from a server-name (eg DECDEV-SQL1) or a server-name and instance name (eg H07251\SQLEXPRESS)
+		/// </summary>
+		/// <param name="server">
+		/// server name and instance name
+		/// </param>
+		public SqlServerInfo(string server, bool getDetails = true)
+		{
+			// is there a back-slash indicating the instance name?
+			var pos = server.IndexOf("\\", StringComparison.Ordinal);
+			if (pos > 0)
+			{
+				this.ServerName = server.Substring(0, pos);
+				this.InstanceName = server.Substring(pos + 1);
+			}
+			else
+			{
+				this.ServerName = server;
+			}
+
+			if (getDetails)
+				// run the query of the database names in another thread:
+				this.DatabaseListTask = Task.Run(() => QueryDatabases());
+			else
+				this.DatabaseListTask = Task.Run(() => { });
+
+		}
+
+		/// <summary>
+		/// construct from the server-discovery row fields (server\instance\version)
+		/// </summary>
+		/// <param name="serverName"></param>
+		/// <param name="instanceName"></param>
+		/// <param name="version"></param>
+		public SqlServerInfo(string serverName, string instanceName, string version, bool getDetails = false)
+		{
+			this.ServerName = serverName;
+			this.InstanceName = instanceName;
+			this.Version = version;
+			if (getDetails)
+				this.DatabaseListTask = Task.Run(() => QueryDatabases());
+			else
+				this.DatabaseListTask = Task.Run(() => { });
+		}
+
+		/// <summary>
+		/// this is the task retrieving the database names from the server
+		/// </summary>
+		public Task DatabaseListTask { get; set; }
+
+		/// <summary>
+		/// the name of the server (computer name)
+		/// </summary>
+		public string ServerName { get; set; } = ""; // don't want this to be able to be null;
+
+		/// <summary>
+		/// the instance name if required
+		/// </summary>
+		public string InstanceName { get; set; }
+
+		/// <summary>
+		/// the version of the server (if known)
+		/// </summary>
+		public string Version { get; set; }
+
+		List<SqlDbInfo> _databases = null;
+
+		/// <summary>
+		/// a list of databases on that server.
+		/// </summary>
+		public List<SqlDbInfo> Databases { get; } = new List<SqlDbInfo>();
+		
+
+		/// <summary>
+		/// pulls out a list of databases on the server.
+		/// </summary>
+		public void QueryDatabases()
+		{
+			try
+			{
+				// create a new connection using the connection string:
+				using (var conn = new SqlConnection(this.ConnectionString))
+				{
+					// open and query for databases:
+					conn.Open();
+					var tbl = conn.GetSchema("Databases");
+
+					// enumerate the databases and create a DBInfo for each row:
+					foreach (DataRow row in tbl.Rows)
+					{
+						// add the db-info to the databases collection.
+						Databases.Add(new SqlDbInfo(ServerName, InstanceName, row.Field<string>("database_name"), true));
+					}
+				}
+			}
+			catch { }
+		}
+
+		/// <summary>
+		/// pulls out a list of databases on the server.
+		/// </summary>
+		public IEnumerable<SqlDbInfo> EnumerateChildren()
+		{
+			// create a new connection using the connection string:
+			using (var conn = new SqlConnection(this.ConnectionString))
+			{
+				// open and query for databases:
+				conn.Open();
+				var tbl = conn.GetSchema("Databases");
+
+				// enumerate the databases and create a DBInfo for each row:
+				foreach (DataRow row in tbl.Rows)
+				{
+					// add the db-info to the databases collection.
+					yield return new SqlDbInfo(ServerName, InstanceName, row.Field<string>("database_name"), false);
+				}
+			}
+		}
+
+		public IEnumerable<SqlDbInfo> Children {  get { return EnumerateChildren(); } }
+
+		public string Description
+		{
+			get {
+				if (!string.IsNullOrEmpty(InstanceName))
+					return $"{ServerName}\\{InstanceName} ({Version})";
+				else
+					return ServerName;
+			}
+		}
+
+		/// <summary>
+		/// uses the <see cref="System.Data.Sql.SqlDataSourceEnumerator"/> to discover all SQL servers visible from the current network location.
+		/// </summary>
+		/// <returns></returns>
+		public static IEnumerable<SqlServerInfo> GetDataSources(bool queryDatabases = true)
+		{
+			// query for SQL servers:
+			var tbl = System.Data.Sql.SqlDataSourceEnumerator.Instance.GetDataSources();
+
+			// build a DbServerInfo for each discovered server:
+			foreach (DataRow row in tbl.Rows)
+			{
+				yield return new SqlServerInfo(
+					row.Field<string>("ServerName"),
+					row.Field<string>("InstanceName"),
+					row.Field<string>("Version"),
+					queryDatabases
+					);
+			}
+		}
+
+		/// <summary>
+		/// compare two <see cref="SqlServerInfo"/> instances for equivalency.
+		/// </summary>
+		/// <param name="other"></param>
+		/// <returns></returns>
+		public bool Equals(SqlServerInfo other)
+		{
+			if (this.ServerName.Equals(other.ServerName, StringComparison.OrdinalIgnoreCase))
+			{
+				return string.Equals(other.InstanceName, InstanceName, StringComparison.OrdinalIgnoreCase);
+			}
+			return false;
+		}
+	}
+
+	/// <summary>
+	/// information about an SQL server database.
+	/// </summary>
+	public class SqlDbInfo 
+	{
+		/// <summary>
+		/// construct the database information.
+		/// </summary>
+		/// <param name="server"></param>
+		/// <param name="instance"></param>
+		/// <param name="db"></param>
+		public SqlDbInfo(string server, string instance, string db, bool fetchDetails = false)
+		{
+			this.ServerName = server;
+			this.InstanceName = instance;
+			this.DBName = db;
+			if (fetchDetails)
+				this.DetailsTask = Task.Run(() => GetDetails());
+			else
+				this.DetailsTask = Task.Run(() => { });
+		}
+
+		/// <summary>
+		/// construct from one of the database records in a <see cref="SqlServerInfo"/>
+		/// </summary>
+		/// <param name="serverInfo"></param>
+		/// <param name="idx"></param>
+		public SqlDbInfo(SqlServerInfo serverInfo, int idx)
+			: this(serverInfo.ServerName, serverInfo.InstanceName, serverInfo.Databases[idx].DBName)
+		{
+
+		}
+
+		public override string ToString()
+		{
+			if (DetailsTask.IsCompleted)
+				return $"{ServerName}.{DBName} ({Tables.Count} tables)";
+			else
+				return $"{ServerName}.{DBName} - Working";
+
+		}
+
+		/// <summary>
+		/// gets the task used to fetch the details of the connection
+		/// </summary>
+		public Task DetailsTask { get; protected set; }
+
+		/// <summary>
+		/// queries the database for a list of tables, and adds the name of each table to the <see cref="Tables"/> collection.
+		/// </summary>
+		void GetDetails()
+		{
+			try
+			{
+				// open a connection
+				using (var conn = new SqlConnection(this.ConnectionString))
+				{
+					conn.Open();
+
+					// query the schema for tables
+					var tables = conn.GetSchema("Tables");
+
+					// add the name of each table to the Tables list:
+					foreach (DataRow row in tables.Rows)
+					{
+						Tables.Add(new SqlDbTableInfo(this, row.Field<string>("TABLE_NAME"), true));
+					}
+				}
+			}
+			catch { }
+		}
+
+
+		public IEnumerable<SqlDbTableInfo> EnumerateChildren()
+		{
+			// open a connection
+			using (var conn = new SqlConnection(this.ConnectionString))
+			{
+				try
+				{
+					conn.Open();
+				}
+				catch
+				{
+					yield break;
+				}
+
+				// query the schema for tables
+				var tables = conn.GetSchema("Tables");
+
+				// add the name of each table to the Tables list:
+				foreach (DataRow row in tables.Rows)
+				{
+					yield return new SqlDbTableInfo(this, row.Field<string>("TABLE_NAME"));
+				}
+			}
+
+		}
+
+		/// <summary>
+		/// children of the database: tables
+		/// </summary>
+		public IEnumerable<SqlDbTableInfo> Children {  get { return EnumerateChildren(); } }
+
+		public string Description
+		{
+			get
+			{
+				return $"{ServerName}.{DBName}";
+			}
+		}
+
+		/// <summary>
+		/// calculates a connection string to the database
+		/// </summary>
+		protected string ConnectionString
+		{
+			get { return $"Data Source={DataSource};Initial Catalog={DBName};Integrated Security=True; Connection Timeout={ConnectionTimeout}"; }
+		}
+
+		/// <summary>
+		/// gets a connection string to the database
+		/// </summary>
+		/// <returns></returns>
+		public string GetConnectionString()
+		{
+			return this.ConnectionString;
+		}
+
+		public SqlConnection CreateConnection()
+		{
+			return new SqlConnection(this.ConnectionString);
+		}
+
+		/// <summary>
+		/// the name of the database
+		/// </summary>
+		public string DBName { get; set; }
+
+		/// <summary>
+		/// the name of the server hosting the database
+		/// </summary>
+		public string ServerName { get; set; }
+
+		/// <summary>
+		/// the name of the instance on the server if any.
+		/// </summary>
+		public string InstanceName { get; set; }
+
+		/// <summary>
+		/// gets or sets the connection timeout part of the connection string.
+		/// </summary>
+		public int ConnectionTimeout { get; set; } = 1;
+
+		/// <summary>
+		/// gets the data source part of the connection string.
+		/// </summary>
+		public string DataSource
+		{
+			get
+			{
+				if (InstanceName != null)
+					return $"{ServerName}\\{InstanceName}";
+				else
+					return ServerName;
+			}
+		}
+
+		/// <summary>
+		/// lists the tables in the database
+		/// </summary>
+		public List<SqlDbTableInfo> Tables { get; } = new List<SqlDbTableInfo>();
+
+	}
+
+	/// <summary>
+	/// information about an SQL server table
+	/// </summary>
+	public class SqlDbTableInfo
+	{
+		public string TableName { get; set; }
+		public int RowCount { get; set; }
+		public SqlDbInfo Database { get; set; }
+		public List<SqlDbColumnInfo> Columns { get; } = new List<SqlDbColumnInfo>();
+
+		public IEnumerable<SqlDbColumnInfo> EnumerateChildren()
+		{
+			using (var connect = Database.CreateConnection())
+			{
+				connect.Open();
+				using (var da = new SqlDataAdapter($"select * from {TableName}", connect))
+				{
+					var dt = new DataTable();
+					try
+					{
+						da.FillSchema(dt, SchemaType.Source);
+					}
+					catch { yield break; }
+
+					foreach (DataColumn col in dt.Columns)
+					{
+						yield return new SqlDbColumnInfo { AllowNulls = col.AllowDBNull, ColumnName = col.ColumnName, DataType = col.DataType.Name, IsKey = dt.PrimaryKey.Contains(col), Ordinal = col.Ordinal };
+					}
+
+				}
+
+			}
+		}
+
+		public IEnumerable<SqlDbColumnInfo> Children
+		{
+			get { return EnumerateChildren(); }
+		}
+
+		public string Description
+		{
+			get { return $".{TableName} Rows: {RowCount}"; }
+		}
+
+		public SqlDbTableInfo(SqlDbInfo db, string tableName, bool fetchDetails = false)
+		{
+			this.Database  = db;
+			this.TableName = tableName;
+
+			try
+			{
+				using (var connect = db.CreateConnection())
+				{
+					connect.Open();
+					using (var cmd = connect.CreateCommand())
+					{
+						cmd.CommandText = $"select count(*) from {tableName}";
+						this.RowCount = Convert.ToInt32(cmd.ExecuteScalar());
+					}
+				}
+			}
+			catch(Exception e) {
+				System.Diagnostics.Debug.Print(e.Message); }
+
+			if (fetchDetails)
+				Task.Run(() => GetDetails(db, tableName));
+		}
+
+		void GetDetails(SqlDbInfo db, string tableName)
+		{
+			this.TableName = tableName;
+			using (var connect = db.CreateConnection())
+			{
+				connect.Open();
+				using (var da = new SqlDataAdapter($"select * from {tableName}", connect))
+				{
+					var dt = new DataTable();
+
+					da.FillSchema(dt, SchemaType.Source);
+
+					foreach (DataColumn col in dt.Columns)
+					{
+						Columns.Add(new SqlDbColumnInfo { AllowNulls = col.AllowDBNull, ColumnName = col.ColumnName, DataType = col.DataType.Name, IsKey = dt.PrimaryKey.Contains(col), Ordinal = col.Ordinal });
+					}
+
+				}
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{TableName} ({RowCount} Rows)";
+		}
+	}
+
+	/// <summary>
+	/// information about an SQL server table column
+	/// </summary>
+	public class SqlDbColumnInfo 
+	{
+		public string ColumnName { get; set; }
+		public string DataType { get; set; }
+		public int Ordinal { get; set; }
+		public bool IsKey { get; set; }
+		public bool AllowNulls { get; set; }
+		public string Description
+		{
+			get { return $"[{ColumnName}] ({DataType}) {(AllowNulls ? "null" : "not null")} {(IsKey ? "(PKEY)" : "")}"; }
+		}
+		public override string ToString()
+		{
+			return $"[{ColumnName}] ({DataType}) {(AllowNulls ? "null" : "not null")} {(IsKey ? "(PKEY)" : "")}";
+		}
+	}
+}
